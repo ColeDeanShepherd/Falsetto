64a4952e5eecacf37a7c086ae9f9bc4d9dbeb4e4
Cole Dean Shepherd <cole@coledeanshepherd.com>
2019-01-21 09:41:51 -0800

    Improved scale chords exercise.

--- TODO.md
+++ TODO.md
@@ -1,10 +1,10 @@
 # To-Do
 ## High Priority
-* Make Scale Chords less cryptic.
 ## Medium Priority
 * Add "more info" links to exercises?
 * Make sequential sounds cut each other off?
 * Post to musictheory subreddit.
+* Stop sending anaytics events in test.
 ## Low Priority
 * Change "Flip to back" terminology?
 * Use actual flat symbols instead of 'b'
--- src/Components/Quizzes/ScaleChords.tsx
+++ src/Components/Quizzes/ScaleChords.tsx
@@ -14,36 +14,20 @@ export function createFlashCardGroup(): FlashCardGroup {
 
 export function createFlashCards(): FlashCard[] {
   return [
-    new FlashCard("Ionian (Major)", "Maj7"),
-    new FlashCard("Dorian", "Min7 (nat. 6)"),
-    new FlashCard("Phrygian", "Min7, Maj∆/7"),
-    new FlashCard("Lydian", "Maj7(#11)"),
-    new FlashCard("Mixolydian", "Dom7"),
-    new FlashCard("Aeolian (Natural Minor)", "Min7(b6)"),
-    new FlashCard("Locrian", "Min7b5"),
-    new FlashCard("Melodic Minor", "MinMaj7"),
-    new FlashCard("Harmonic Minor", "Minmaj7, o∆/b7"),
-
-    new FlashCard("Tonic Diminished", "Dim7, Maj∆/b9"),
-    new FlashCard("Dominant Diminished", "Dom13,b9,#9,b5"),
-    new FlashCard("Whole Tone", "Dom7,#5,b5"),
-    new FlashCard("Augmented", "Aug∆/7Aug∆"),
-    new FlashCard("Major Pentatonic", "Maj(6,7)"),
-    new FlashCard("Minor Pentatonic", "Min(7,11)"),
-    new FlashCard("Major Blues", "Dom7, Maj(6,7)"),
-    new FlashCard("Minor Blues", "Min7, Dom7#9"),
-    
-    new FlashCard("Dorian b2", "Min7sus4b9"),
-    new FlashCard("Lydian Aug.", "Maj7#4#5, Maj∆/b6"),
-    new FlashCard("Mixolydian #11", "Dom7b5"),
-    new FlashCard("Mixolydian b6", "Dom7b6"),
-    new FlashCard("Locrian Nat. 9", "Min9b6"),
-    new FlashCard("Altered Dominant", "Dom7b9,#9,b5,#5"),
-    new FlashCard("Locrian Nat. 6", "Min7b5"),
-    new FlashCard("Ionian Aug.", "Maj7sus4,#5"),
-    new FlashCard("Dorian #11", "Min7(#11)"),
-    new FlashCard("Phrygian Major", "Dom7sus4,b9, #5"),
-    new FlashCard("Lydian #9", "Maj7#9,#11, Maj∆/b9"),
-    new FlashCard("Altered Dominant bb7", "Dimo7")
+    new FlashCard("Ionian", "Maj13"),
+    new FlashCard("Dorian", "m13"),
+    new FlashCard("Phrygian", "m7♭9♭13"),
+    new FlashCard("Lydian", "Maj13#11"),
+    new FlashCard("Mixolydian", "13"),
+    new FlashCard("Aeolian", "m11♭13"),
+    new FlashCard("Locrian", "m7♭5♭9♭13"),
+    new FlashCard("Melodic minor", "mMaj7"),
+    new FlashCard("Harmonic minor", "mMaj7b13"),
+    new FlashCard("Phrygian ♮6", "m13♭9"),
+    new FlashCard("Lydian Augmented", "Maj13#5#11"),
+    new FlashCard("Lydian Dominant", "13#11"),
+    new FlashCard("Mixolydian b6", "11♭13"),
+    new FlashCard("Half-diminished", "m7♭5♭13"),
+    new FlashCard("Altered", "7♭5♭9#9♭13 (7alt)")
   ];
 }
