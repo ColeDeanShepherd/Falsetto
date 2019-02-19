8f73fe13d798ce5d61da3a798814347b2aa99470
Cole Dean Shepherd <cole@coledeanshepherd.com>
2019-02-19 07:38:57 -0800

    Added rests to note durations exercise.

--- src/Components/Quizzes/NoteDurations.tsx
+++ src/Components/Quizzes/NoteDurations.tsx
@@ -12,7 +12,7 @@ const height = 100;
 
 export function createFlashCardGroup(): FlashCardGroup {
   const flashCardGroup = new FlashCardGroup("Note Durations", createFlashCards());
-  flashCardGroup.initialSelectedFlashCardIndices = Utils.range(0, 4);
+  flashCardGroup.initialSelectedFlashCardIndices = Utils.range(0, 4).concat(Utils.range(8, 12));
   flashCardGroup.renderAnswerSelect = FlashCardUtils.renderDistinctFlashCardSideAnswerSelect;
 
   return flashCardGroup;
@@ -26,7 +26,7 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "w", null)}
         />
       ),
-      "Whole Note = 4 beats"
+      "Whole Note - 4 beats"
     ),
     FlashCard.fromRenderFns(
       () => (
@@ -35,7 +35,7 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "h", ["hr"])}
         />
       ),
-      "Half Note = 2 beats"
+      "Half Note - 2 beats"
     ),
     FlashCard.fromRenderFns(
       () => (
@@ -44,7 +44,7 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "q", ["qr", "hr"])}
         />
       ),
-      "Quarter Note = 1 beat"
+      "Quarter Note - 1 beat"
     ),
     FlashCard.fromRenderFns(
       () => (
@@ -53,7 +53,7 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "8", ["8r", "qr", "hr"])}
         />
       ),
-      "Eighth Note = 1/2 beats"
+      "Eighth Note - 1/2 beats"
     ),
     FlashCard.fromRenderFns(
       () => (
@@ -62,7 +62,7 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "16", ["16r", "8r", "qr", "hr"])}
         />
       ),
-      "Sixteenth Note = 1/4 beats"
+      "Sixteenth Note - 1/4 beats"
     ),
     FlashCard.fromRenderFns(
       () => (
@@ -71,7 +71,7 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "32", ["32r", "16r", "8r", "qr", "hr"])}
         />
       ),
-      "32nd Note = 1/8 beats"
+      "32nd Note - 1/8 beats"
     ),
     FlashCard.fromRenderFns(
       () => (
@@ -80,7 +80,7 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "64", ["64r", "32r", "16r", "8r", "qr", "hr"])}
         />
       ),
-      "64th Note = 1/16 beats"
+      "64th Note - 1/16 beats"
     ),
     FlashCard.fromRenderFns(
       () => (
@@ -89,7 +89,79 @@ export function createFlashCards(): FlashCard[] {
           vexFlowRender={vexFlowRender.bind(null, "128", ["128r", "64r", "32r", "16r", "8r", "qr", "hr"])}
         />
       ),
-      "128th Note = 1/32 beats"
+      "128th Note - 1/32 beats"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "wr", null)}
+        />
+      ),
+      "Whole Rest - 4 beats"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "hr", ["hr"])}
+        />
+      ),
+      "Half Rest - 2 beats"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "qr", ["qr", "hr"])}
+        />
+      ),
+      "Quarter Rest - 1 beat"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "8r", ["8r", "qr", "hr"])}
+        />
+      ),
+      "Eighth Rest - 1/2 beats"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "16r", ["16r", "8r", "qr", "hr"])}
+        />
+      ),
+      "Sixteenth Rest - 1/4 beats"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "32r", ["32r", "16r", "8r", "qr", "hr"])}
+        />
+      ),
+      "32nd Rest - 1/8 beats"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "64r", ["64r", "32r", "16r", "8r", "qr", "hr"])}
+        />
+      ),
+      "64th Rest - 1/16 beats"
+    ),
+    FlashCard.fromRenderFns(
+      () => (
+        <VexFlowComponent
+          width={width} height={height}
+          vexFlowRender={vexFlowRender.bind(null, "128r", ["128r", "64r", "32r", "16r", "8r", "qr", "hr"])}
+        />
+      ),
+      "128th Rest - 1/32 beats"
     )
   ];
 }
@@ -99,8 +171,7 @@ function vexFlowRender(noteDurationString: string, restDurationStrings: string[]
     .setBackgroundFillStyle("#eed");
 
   const stave = new Vex.Flow.Stave(0, 0, width);
-  stave.addClef("treble").addTimeSignature("4/4");
-  //stave.setContext(context).draw();
+  stave.setContext(context).draw();
   
   const notes = [
     new Vex.Flow.StaveNote({
@@ -110,17 +181,19 @@ function vexFlowRender(noteDurationString: string, restDurationStrings: string[]
     })
   ];
 
-  notes[0].setXShift(-30);
+  notes[0].setXShift(25);
 
   if (restDurationStrings) {
     for (const durationString of restDurationStrings) {
-      notes.push(
-        new Vex.Flow.StaveNote({
-          clef: "treble",
-          keys: ["b/4"],
-          duration: durationString
-        })
-      );
+      const restNote = new Vex.Flow.StaveNote({
+        clef: "treble",
+        keys: ["b/4"],
+        duration: durationString
+      });
+
+      notes.push(restNote);
+
+      restNote.setXShift(390);
     }
   }
   
