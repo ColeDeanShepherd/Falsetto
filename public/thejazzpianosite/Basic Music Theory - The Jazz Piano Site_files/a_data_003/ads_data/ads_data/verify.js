dd0a9cc72f8bc3fbf06444530f375bacce67ce06
Cole Shepherd <cole.shepherd@againstgrav.com>
2019-04-03 21:10:54 -0700

    Fixed thejazzpianosite demo page.

---
+++ public/thejazzpianosite/Basic Music Theory - The Jazz Piano Site_files/a_data_003/ads_data/ads_data/verify.js
@@ -0,0 +1,12 @@
+var dvObj = $dvbs;function np764531(g,i){function d(){function d(){function f(b,a){b=(i?"dvp_":"")+b;e[b]=a}var e={},a=function(b){for(var a=[],c=0;c<b.length;c+=2)a.push(String.fromCharCode(parseInt(b.charAt(c)+b.charAt(c+1),32)));return a.join("")},h=window[a("3e313m3937313k3f3i")];h&&(a=h[a("3g3c313k363f3i3d")],f("pltfrm",a));(function(){var a=e;e={};dvObj.registerEventCall(g,a,2E3,true)})()}try{d()}catch(f){}}try{dvObj.pubSub.subscribe(dvObj==window.$dv?"ImpressionServed":"BeforeDecisionRender",g,"np764531",d)}catch(f){}}
+;np764531("b3e3812d88384b5a9f452aba3b856a49",false);
+
+
+try{__tagObject_callback_734656587242({ImpressionID:"b3e3812d88384b5a9f452aba3b856a49", ServerPublicDns:"tps713.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "b3e3812d88384b5a9f452aba3b856a49");}catch(e){}
+try{__verify_callback_734656587242({
+ResultID:1,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "b3e3812d88384b5a9f452aba3b856a49");}catch(e){}
---
+++ public/thejazzpianosite/Basic Music Theory - The Jazz Piano Site_files/elementor.js
@@ -0,0 +1,1 @@
+jQuery(function($){$("#elementor-panel-footer-templates").addClass('OKKKKKKKK');});
