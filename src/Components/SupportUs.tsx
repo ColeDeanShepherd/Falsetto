068839c47430af083ee193b30a3b0bd5f8568f58
Cole Shepherd <cole.shepherd@againstgrav.com>
2019-05-11 12:58:15 -0700

    Fixed "Become a Patron!" button.

--- src/Components/SupportUs.tsx
+++ src/Components/SupportUs.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { CardContent, Card, Typography } from "@material-ui/core";
 import { MAX_MAIN_CARD_WIDTH } from './Style';
 
+import becomeAPatronButton from "../img/become_a_patron_button.png";
+
 export class SupportUsPage extends React.Component<{}, {}> {
   public render(): JSX.Element {
     return (
@@ -11,7 +13,11 @@ export class SupportUsPage extends React.Component<{}, {}> {
             Support Us
           </Typography>
           <p>I combine my passions for music and software to create free, interactive music theory lessons and exercises. Balancing a day job to pay the bills and a passion project can be tough, and your patronage can help me allocate more of my time towards creating and improving music theory lessons without resorting to a paywall or intrusive advertisements. With your support, my goal is to make falsetto.app the #1 resource for learning music theory online.</p>
-          <p style={{ textAlign: "center" }}><a href="https://www.patreon.com/bePatron?u=4644571" data-patreon-widget-type="become-patron-button">Become a Patron!</a></p>
+          <p style={{ textAlign: "center" }}>
+            <a href="https://www.patreon.com/bePatron?u=4644571" target="_blank">
+              <img src={becomeAPatronButton} alt="Become a Patron!" style={{width: "176px", borderRadius: "9999px"}} />
+            </a>
+          </p>
         </CardContent>
       </Card>
     );
