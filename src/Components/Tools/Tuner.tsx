2d524397b8d3a7e568a20c7c25c4bc5b3b869baf
Cole Shepherd <cole@coledeanshepherd.com>
2019-08-25 12:49:21 -0700

    Finished V1 of the tuner.

--- src/Components/Tools/Tuner.tsx
+++ src/Components/Tools/Tuner.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, Typography } from "@material-ui/core";
 import * as Utils from "../../Utils";
 import { Pitch } from '../../Pitch';
 import { detectPitch } from '../PitchDetection';
+import { Size2D } from '../../Size2D';
 
 export class Microphone {
   public mediaStream: MediaStream | null = null;
@@ -70,6 +71,52 @@ export class Microphone {
   }
 }
 
+export interface ITunerCentsIndicatorProps {
+  detuneCents: number;
+}
+export interface ITunerCentsIndicatorState {}
+
+export class TunerCentsIndicator
+  extends React.Component<ITunerCentsIndicatorProps, ITunerCentsIndicatorState>
+{
+  public render(): JSX.Element {
+    // svg horizontal scale
+    const svgSize = new Size2D(100, 5);
+    const style: any = {
+      width: "100%",
+      height: "5px"
+    };
+
+    const { detuneCents } = this.props;
+
+    const centsInHalfStep = 100;
+    const centsBarWidthPct = Math.abs(detuneCents) / (centsInHalfStep / 2);
+    const centsBarWidth = centsBarWidthPct * (svgSize.width / 2);
+    const centsBarX = (detuneCents > 0)
+      ? (svgSize.width / 2)
+      : ((svgSize.width / 2) - centsBarWidth);
+
+    return (
+      <svg
+        width={svgSize.width} height={svgSize.height}
+        viewBox={`0 0 ${svgSize.width} ${svgSize.height}`}
+        version="1.1" xmlns="http://www.w3.org/2000/svg"
+        style={style}
+      >
+        <rect
+          x={0} y={0}
+          width={svgSize.width} height={svgSize.height}
+          fill="gray"
+        />
+        <rect
+          x={centsBarX} y={0}
+          width={centsBarWidth} height={svgSize.height}
+          fill="lightgreen"
+        />
+      </svg>
+    );
+  }
+}
 
 const fftSize = 2048;
 
@@ -119,7 +166,7 @@ export class Tuner extends React.Component<ITunerProps, ITunerState> {
 
   public render(): JSX.Element {
     return (
-      <Card>
+      <Card style={{ maxWidth: "420px", margin: "0 auto", textAlign: "center" }}>
         <CardContent>
           <div style={{display: "flex"}}>
             <Typography gutterBottom={true} variant="h5" component="h2" style={{flexGrow: 1}}>
@@ -129,12 +176,17 @@ export class Tuner extends React.Component<ITunerProps, ITunerState> {
 
           <div>
             {this.state.detectedPitch ? (
-              <div>{this.state.detectedPitch.toOneAccidentalAmbiguousString(true, true)}</div>
+              <div style={{ fontSize: "2em" }}>{this.state.detectedPitch.toOneAccidentalAmbiguousString(true, true)}</div>
             ) : null}
             {this.state.detectedPitchDetuneCents ? (
               <div>
-                {(this.state.detectedPitchDetuneCents > 0) ? "+" : ""}
-                {this.state.detectedPitchDetuneCents} cents
+                <div>
+                  <TunerCentsIndicator detuneCents={this.state.detectedPitchDetuneCents} />
+                </div>
+                <div>
+                  {(this.state.detectedPitchDetuneCents > 0) ? "+" : ""}
+                  {this.state.detectedPitchDetuneCents} cents
+                </div>
               </div>
             ) : null}
           </div>
