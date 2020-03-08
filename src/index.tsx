55e7815828e651cc0b9dd3c303fe4f0eba2bce79
Cole Shepherd <cole@coledeanshepherd.com>
2020-03-07 20:53:53 -0800

    Merge progress.

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,20 @@ import "core-js";
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./Components/App";
+import { AppView } from "./App/View";
 import "./index.css";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
 import { polyfillWebAudio } from "./Audio";
 import registerServiceWorker from "./registerServiceWorker";
-import { isDevelopment } from './Config';
+import { Auth0Provider } from "./react-auth0-wrapper";
+import { authDomain, authClientId, authAudience, isDevelopment } from './Config';
+import "whatwg-fetch";
+import { AppModel } from './App/Model';
 import { checkFlashCardSetIds, checkFlashCardIds } from './FlashCardGraph';
 import { getErrorDescription } from './Error';
 import { DependencyInjector } from './DependencyInjector';
 import { IAnalytics } from './Analytics';
+import { Auth0InjectorView } from './Auth0Injector/Auth0InjectorView';
 
 const analytics = DependencyInjector.instance.getRequiredService<IAnalytics>("IAnalytics");
 
@@ -48,6 +52,13 @@ if (isDevelopment()) {
   checkFlashCardSetIds();
   checkFlashCardIds();
 }
+
+const app = new AppModel();
+
+window.addEventListener("beforeunload", (e) => {
+  app.dispose();
+});
+
 // A function that routes the user to the right place
 // after login
 // TODO: uncomment when authentication is re-enabled
@@ -59,11 +70,28 @@ if (isDevelopment()) {
       ? appState.targetUrl
       : window.location.pathname
   );
-};*/
+};
+
+const rootElement = (
+  <Auth0Provider
+    onRedirectCallback={onRedirectCallback}
+    initOptions={{
+      domain: authDomain,
+      client_id: authClientId,
+      redirect_uri: window.location.origin,
+      audience: authAudience
+    }}
+  >
+    <Auth0InjectorView />
+    <MuiThemeProvider theme={theme}>
+      <AppView />
+    </MuiThemeProvider>
+  </Auth0Provider>
+);*/
 
 const rootElement = (
   <MuiThemeProvider theme={theme}>
-    <App isEmbedded={false} />
+    <AppView />
   </MuiThemeProvider>
 );
 
