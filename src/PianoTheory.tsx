82565a4b1639a4197bf97b116da2f24ae86873e4
Cole Shepherd <cole@recroom.com>
2020-03-16 07:58:53 -0700

    Progress on PianoTheory.

--- src/PianoTheory.tsx
+++ src/PianoTheory.tsx
@@ -22,26 +22,60 @@ class KeyBinding {
 
 // TODO: dynamic width/height
 // TODO: quizzes
+// TODO: slide links
+// TODO: use symbols
 const slideGroups = [
   new SlideGroup("Introduction", [
-    new Slide(() => <span>Introduction to format</span>),
-    new Slide(() => <span>This is a piano</span>),
-    new Slide(() => <span>There are 88 white &amp; black keys on a standard piano. Some pianos/keyboards are smaller.</span>),
-    new Slide(() => <span>These lessons will be on a subset of the full piano.</span>),
+    new Slide(() => <span>Introduction to format (when format is determined)</span>),
+    new Slide(() => <span>This is a piano (when figure out start &amp; end)</span>),
+    new Slide(() => <span>Pianos are made of white &amp; black "keys".</span>),
+    new Slide(() => <span>Standard pianos have 88 white &amp; black keys. Some pianos/keyboards are smaller.</span>),
+    new Slide(() => <span>These lessons will generally only show a portion of the 88 keys due to screen size limitations.</span>),
 
-    new Slide(() => <span>Each key on a piano plays a pitch. Defn. of pitch. Try clicking keys</span>),
-    new Slide(() => <span>Keys on the left produce lower pitches. Keys on the right produce higher pitches.</span>),
+    new Slide(() => <span>Each key produces a particular "pitch" &ndash; the "highness" or "lowness" of a sound &ndash; when pressed. Try pressing some of the keys on your screen, or try connecting a MIDI keyboard and physically pressing keys, to hear the pitches they produce.</span>),
+    new Slide(() => <span>Keys further to the left produce lower pitches, and keys further to the right produce higher pitches.</span>),
+
+    // quiz
+
     new Slide(() => <span>Each pitch has a specific name</span>),
-    new Slide(() => <span>Pitch names repeat</span>),
-    new Slide(() => <span>White key pitches &amp; names.</span>),
-    new Slide(() => <span>Black key pitches &amp; names.</span>),
-    new Slide(() => <span></span>),
-    new Slide(() => <span></span>),
-    new Slide(() => <span></span>),
-    new Slide(() => <span></span>),
-    new Slide(() => <span></span>),
-    new Slide(() => <span></span>),
+    new Slide(() => <span>Let's first focus on the white keys in a small section of the keyboard.</span>),
+    new Slide(() => <span>This key, immediately to the left of the group of 3 black keys, is called C.</span>),
+    new Slide(() => <span>This key is called D.</span>),
+    new Slide(() => <span>This key is called E.</span>),
+    new Slide(() => <span>This key is called F.</span>),
+    new Slide(() => <span>This key is called G.</span>),
+    new Slide(() => <span>This key is called A.</span>),
+    new Slide(() => <span>This key is called B.</span>),
+    // quiz
+    
+    new Slide(() => <span>Now let's learn the names of the black keys.</span>),
+    new Slide(() => <span>This key, like all black keys, has multiple names. One of the names for it is C#. Another name for it is Db. The reason for multiple names will be explained later.</span>),
+    new Slide(() => <span>Sharps &amp; flats explanation.</span>),
+    new Slide(() => <span>This key is called D#, or Eb.</span>),
+    new Slide(() => <span>This key is called F#, or Gb.</span>),
+    new Slide(() => <span>This key is called G#, or Ab.</span>),
+    new Slide(() => <span>This key is called A#, or Bb.</span>),
+    // quiz
 
+    new Slide(() => <span>Now that we've learned the names of the keys in the section, let's zoom out.</span>),
+    new Slide(() => <span>You can see that the piano keys are simply a repetition of the pattern we've learned.</span>),
+    new Slide(() => <span>Each repetition of the pattern has the same pitch names, so we've now learned all the notes on the piano!</span>),
+    // quiz?
+
+    new Slide(() => <span>Now let's learn about scales.</span>),
+    new Slide(() => <span>Scales are ...</span>),
+    new Slide(() => <span>Most music is based on only a few scales.</span>),
+    new Slide(() => <span>Here is an example of a scale.</span>),
+    new Slide(() => <span>C major scale</span>),
+
+    new Slide(() => <span>Here is another example of a scale.</span>),
+    new Slide(() => <span>Bb major scale.</span>),
+    
+    new Slide(() => <span>Both scales are classified as "major" scales, and have the same "formula".</span>),
+    new Slide(() => <span>Major scale formula.</span>),
+    
+    new Slide(() => <span>You can derive other major scales with the formula.</span>),
+    new Slide(() => <span>Cover all the other scales.</span>),
 
 
 
