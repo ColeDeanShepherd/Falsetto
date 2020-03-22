52cfacef12b4f3f6dcdaa86f1085d949257eb38b
Cole Shepherd <cole@recroom.com>
2020-03-21 17:20:07 -0700

    Improvements to PianoTheory slideshow.

--- src/PianoTheory.tsx
+++ src/PianoTheory.tsx
@@ -5,8 +5,45 @@ import { Size2D } from './lib/Core/Size2D';
 import { Vector2D } from './lib/Core/Vector2D';
 import { Pitch } from './lib/TheoryLib/Pitch';
 import { PitchLetter } from './lib/TheoryLib/PitchLetter';
-import { playPitches } from './Audio/GuitarAudio';
+import { playPitches } from './Audio/PianoAudio';
 import { flattenArrays } from './lib/Core/ArrayUtils';
+
+export const FullPiano: React.FunctionComponent<{}> = props => (
+  <PianoKeyboard
+    rect={new Rect2D(new Size2D(400, 50), new Vector2D(0, 0))}
+    lowestPitch={new Pitch(PitchLetter.A, 0, 0)}
+    highestPitch={new Pitch(PitchLetter.C, 0, 8)}
+    onKeyPress={p => playPitches([p])}
+    style={{ width: "100%", maxWidth: "400px", height: "auto" }} />
+);
+
+export const TwoOctavePiano: React.FunctionComponent<{}> = props => (
+  <PianoKeyboard
+    rect={new Rect2D(new Size2D(300, 100), new Vector2D(0, 0))}
+    lowestPitch={new Pitch(PitchLetter.C, 0, 3)}
+    highestPitch={new Pitch(PitchLetter.B, 0, 4)}
+    onKeyPress={p => playPitches([p])}
+    style={{ width: "100%", maxWidth: "300px", height: "auto" }} />
+);
+
+export const OneOctavePiano: React.FunctionComponent<{}> = props => (
+  <PianoKeyboard
+    rect={new Rect2D(new Size2D(150, 100), new Vector2D(0, 0))}
+    lowestPitch={new Pitch(PitchLetter.C, 0, 4)}
+    highestPitch={new Pitch(PitchLetter.B, 0, 4)}
+    onKeyPress={p => playPitches([p])}
+    style={{ width: "100%", maxWidth: "300px", height: "auto" }} />
+);
+
+export const PianoNoteDiagram: React.FunctionComponent<{ pitch: Pitch }> = props => (
+  <PianoKeyboard
+    rect={new Rect2D(new Size2D(150, 100), new Vector2D(0, 0))}
+    lowestPitch={new Pitch(PitchLetter.C, 0, 4)}
+    highestPitch={new Pitch(PitchLetter.B, 0, 4)}
+    onKeyPress={p => playPitches([p])}
+    pressedPitches={[props.pitch]}
+    style={{ width: "100%", maxWidth: "300px", height: "auto" }} />
+);
 
 class Slide {
   public constructor(public renderFn: () => JSX.Element) {}
@@ -27,39 +64,176 @@ class KeyBinding {
 const slideGroups = [
   new SlideGroup("Introduction", [
     new Slide(() => <span>Introduction to format (when format is determined)</span>),
-    new Slide(() => <span>This is a piano (when figure out start &amp; end)</span>),
-    new Slide(() => <span>Pianos are made of white &amp; black "keys".</span>),
-    new Slide(() => <span>Standard pianos have 88 white &amp; black keys. Some pianos/keyboards are smaller.</span>),
-    new Slide(() => <span>These lessons will generally only show a portion of the 88 keys due to screen size limitations.</span>),
+    new Slide(() => (
+      <div>
+        <p>This is a piano.</p>
+        <FullPiano />
+      </div>
+    )),
+    new Slide(() => (
+      <div>
+        <p>Pianos are made of white &amp; black "keys".</p>
+        <FullPiano />
+      </div>
+    )),
+    new Slide(() => (
+      <div>
+        <p>Standard pianos have 88 white &amp; black keys. Some pianos/keyboards are smaller.</p>
+        <FullPiano />
+      </div>
+    )),
+    new Slide(() => (
+      <div>
+        <p>These lessons will generally only show a portion of the 88 keys due to screen size limitations.</p>
+        <TwoOctavePiano />
+      </div>
+    )),
 
-    new Slide(() => <span>Each key produces a particular "pitch" &ndash; the "highness" or "lowness" of a sound &ndash; when pressed. Try pressing some of the keys on your screen, or try connecting a MIDI keyboard and physically pressing keys, to hear the pitches they produce.</span>),
-    new Slide(() => <span>Keys further to the left produce lower pitches, and keys further to the right produce higher pitches.</span>),
+    new Slide(() => (
+      <div>
+        <p>Each key produces a particular "pitch" &ndash; the "highness" or "lowness" of a sound &ndash; when pressed. Try pressing some of the keys on your screen, or try connecting a MIDI keyboard and physically pressing keys, to hear the pitches they produce.</p>
+        <TwoOctavePiano />
+      </div>
+    )),
+    new Slide(() => (
+      <div>
+        <p>Keys further to the left produce lower pitches, and keys further to the right produce higher pitches.</p>
+        <TwoOctavePiano />
+      </div>
+    )),
 
     // quiz
 
-    new Slide(() => <span>Each pitch has a specific name</span>),
-    new Slide(() => <span>Let's first focus on the white keys in a small section of the keyboard.</span>),
-    new Slide(() => <span>This key, immediately to the left of the group of 3 black keys, is called C.</span>),
-    new Slide(() => <span>This key is called D.</span>),
-    new Slide(() => <span>This key is called E.</span>),
-    new Slide(() => <span>This key is called F.</span>),
-    new Slide(() => <span>This key is called G.</span>),
-    new Slide(() => <span>This key is called A.</span>),
-    new Slide(() => <span>This key is called B.</span>),
+    new Slide(() => (
+      <div>
+        <p>Each pitch has a specific name.</p>
+        <TwoOctavePiano />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>Let's first focus on the white keys in a small section of the keyboard.</p>
+        <OneOctavePiano />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key, immediately to the left of the group of 3 black keys, is called C.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.C, 0, 4)} />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key is called D.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.D, 0, 4)} />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key is called E.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.E, 0, 4)} />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key is called F.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.F, 0, 4)} />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key is called G.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.G, 0, 4)} />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key is called A.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.A, 0, 4)} />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key is called B.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.B, 0, 4)} />
+      </div>
+    )),
     // quiz
     
     new Slide(() => <span>Now let's learn the names of the black keys.</span>),
-    new Slide(() => <span>This key, like all black keys, has multiple names. One of the names for it is C#. Another name for it is Db. The reason for multiple names will be explained later.</span>),
-    new Slide(() => <span>Sharps &amp; flats explanation.</span>),
-    new Slide(() => <span>This key is called D#, or Eb.</span>),
-    new Slide(() => <span>This key is called F#, or Gb.</span>),
-    new Slide(() => <span>This key is called G#, or Ab.</span>),
-    new Slide(() => <span>This key is called A#, or Bb.</span>),
+    
+    new Slide(() => (
+      <div>
+        <p>This key, like all black keys, has multiple names. One of the names for it is C#. Another name for it is Db. The reason for multiple names will be explained later.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.C, 1, 4)} />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>Sharps &amp; flats explanation.</p>
+        <OneOctavePiano />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>This key is called D#, or Eb.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.D, 1, 4)} />
+      </div>
+    )),
+
+    new Slide(() => (
+      <div>
+        <p>This key is called F#, or Gb.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.F, 1, 4)} />
+      </div>
+    )),
+
+    new Slide(() => (
+      <div>
+        <p>This key is called G#, or Ab.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.G, 1, 4)} />
+      </div>
+    )),
+
+    new Slide(() => (
+      <div>
+        <p>This key is called A#, or Bb.</p>
+        <PianoNoteDiagram pitch={new Pitch(PitchLetter.B, -1, 4)} />
+      </div>
+    )),
     // quiz
 
-    new Slide(() => <span>Now that we've learned the names of the keys in the section, let's zoom out.</span>),
-    new Slide(() => <span>You can see that the piano keys are simply a repetition of the pattern we've learned.</span>),
-    new Slide(() => <span>Each repetition of the pattern has the same pitch names, so we've now learned all the notes on the piano!</span>),
+    new Slide(() => (
+      <div>
+        <p>Now that we've learned the names of the keys in the section, let's zoom out.</p>
+        <TwoOctavePiano />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>You can see that the piano keys are simply a repetition of the pattern we've learned.</p>
+        <TwoOctavePiano />
+      </div>
+    )),
+    
+    new Slide(() => (
+      <div>
+        <p>Each repetition of the pattern has the same pitch names, so we've now learned all the notes on the piano!</p>
+        <TwoOctavePiano />
+      </div>
+    )),
+
     // quiz?
 
     new Slide(() => <span>Now let's learn about scales.</span>),
@@ -139,7 +313,7 @@ export class PianoTheory extends React.Component<IPianoTheoryProps, IPianoTheory
     const renderedSlide = slides[slideIndex].renderFn();
 
     return (
-      <div style={{ width: "100%", height: "100vh" }}>
+      <div style={{ width: "100%", height: "100vh", textAlign: "center" }}>
         <div>{slideNumber} / {numSlides}</div>
         {renderedSlide}
       </div>
