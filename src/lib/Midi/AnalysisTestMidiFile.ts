73b53c829ecf53aeea11b498492e36effe5dabd4
Cole Shepherd <cole@coledeanshepherd.com>
2020-08-06 22:06:07 -0700

    Additions to analysis test file.

--- src/lib/Midi/AnalysisTestMidiFile.ts
+++ src/lib/Midi/AnalysisTestMidiFile.ts
@@ -1,4 +1,4 @@
-import { Midi } from "@tonejs/midi";
+import { Midi, Track } from "@tonejs/midi";
 
 import { addAscendingScale, addDescendingScale, addChord, addAscendingArpeggio, addDescendingArpeggio } from "./MidiGeneration";
 import { Scale, ScaleType } from "../TheoryLib/Scale";
@@ -7,6 +7,34 @@ import { PitchLetter } from "../TheoryLib/PitchLetter";
 import { Chord } from "../TheoryLib/Chord";
 import { ChordType } from "../TheoryLib/ChordType";
 
+function addTwoHanded251(
+  midiTrack: Track,
+  scale: Scale,
+  startTimeTicks: number,
+  chordDurationTicks: number
+) {
+  const chords = [
+    scale.getDiatonicChord(2, 4),
+    scale.getDiatonicChord(5, 4),
+    scale.getDiatonicChord(1, 4)
+  ];
+
+  for (const chord of chords) {
+    chord.rootPitch.octaveNumber -= 2;
+  }
+
+  let chordStartTimeTicks = startTimeTicks;
+
+  for (const chord of chords) {
+    const chordEndTimeTicks = chordStartTimeTicks + chordDurationTicks;
+
+    addChord(midiTrack, chord, chordStartTimeTicks, chordDurationTicks);
+    addAscendingScale(midiTrack, scale, chordStartTimeTicks, chordDurationTicks / scale.type.numPitches);
+
+    chordStartTimeTicks = chordEndTimeTicks;
+  }
+}
+
 export function createAnalysisTestMidi(): Midi {
   const midi = new Midi();
   midi.header.setTempo(256);
@@ -14,28 +42,51 @@ export function createAnalysisTestMidi(): Midi {
   const testSpacingTicks = midi.header.ppq * 2;
 
   const track = midi.addTrack();
+  
+  addTwoHanded251(
+    track,
+    new Scale(ScaleType.Major, new Pitch(PitchLetter.C, 0, 5)),
+    /*startTimeTicks*/ 0,
+    /*durationTicks*/ 8 * midi.header.ppq);
+    
+    addTwoHanded251(
+    track,
+    new Scale(ScaleType.Major, new Pitch(PitchLetter.E, 0, 4)),
+    /*startTimeTicks*/ track.durationTicks + testSpacingTicks,
+    /*durationTicks*/ 8 * midi.header.ppq);
+  
   addAscendingScale(
     track,
     new Scale(ScaleType.Major, new Pitch(PitchLetter.C, 0, 4)),
-    /*startTimeTicks*/ 0,
+    /*startTimeTicks*/ track.durationTicks + testSpacingTicks,
     /*durationTicks*/ midi.header.ppq);
+
   addDescendingScale(
     track,
     new Scale(ScaleType.Major, new Pitch(PitchLetter.E, 0, 4)),
     /*startTimeTicks*/ track.durationTicks + testSpacingTicks,
     /*durationTicks*/ midi.header.ppq);
+  
+  addAscendingScale(
+    track,
+    new Scale(ScaleType.Chromatic, new Pitch(PitchLetter.C, 0, 4)),
+    /*startTimeTicks*/ track.durationTicks + testSpacingTicks,
+    /*durationTicks*/ midi.header.ppq);
+
   addChord(
     track,
     new Chord(ChordType.Dom7, new Pitch(PitchLetter.A, 0, 3)),
     /*startTimeTicks*/ track.durationTicks + testSpacingTicks,
     /*durationTicks*/ 4 * midi.header.ppq
   );
+
   addAscendingArpeggio(
     track,
     new Chord(ChordType.Dom7, new Pitch(PitchLetter.F, 0, 3)),
     /*startTimeTicks*/ track.durationTicks + testSpacingTicks,
     /*durationTicks*/ midi.header.ppq
   );
+
   addDescendingArpeggio(
     track,
     new Chord(ChordType.Dom7, new Pitch(PitchLetter.G, 0, 3)),
