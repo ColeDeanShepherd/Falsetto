89e1a49d7d39a7d3d0076cd42cf499285a9d3598
Cole Shepherd <cole@recroom.com>
2022-10-15 17:15:44 -0700

    Improved guitar chords.

--- src/lib/Core/Dbc.ts
+++ src/lib/Core/Dbc.ts
@@ -1,17 +1,10 @@
-// TODO: don't run asserts in prod?
-// TODO: add tests
+// Design by contract utilities.
+
 export function assert(condition: boolean) {
   if (!condition) {
     throw new Error("Failed assertion.");
   }
 }
-
-// TODO: add tests
-export function precondition(condition: boolean) {
-  assert(condition);
-}
-
-// TODO: add tests
-export function invariant(condition: boolean) {
-  assert(condition);
-}
+export const precondition = (condition: boolean) => assert(condition);
+export const postcondition = (condition: boolean) => assert(condition);
+export const invariant = (condition: boolean) => assert(condition);
--- src/ui/Quizzes/Chords/GuitarChords.tsx
+++ src/ui/Quizzes/Chords/GuitarChords.tsx
@@ -1,172 +1,218 @@
 import * as React from "react";
-import { TableRow, TableCell, Table, TableHead, TableBody, Grid, Checkbox } from "@material-ui/core";
 
-import { Size2D } from "../../../lib/Core/Size2D";
-import { FlashCard, FlashCardSide, FlashCardId, createFlashCardId } from "../../../FlashCard";
-import { FlashCardSet, FlashCardStudySessionInfo, FlashCardLevel } from "../../../FlashCardSet";
-import { Pitch } from "../../../lib/TheoryLib/Pitch";
-import { PitchLetter } from "../../../lib/TheoryLib/PitchLetter";
-import { ChordType, chordTypeLevels } from "../../../lib/TheoryLib/ChordType";
-import {  renderDistinctFlashCardSideAnswerSelect } from '../Utils';
-import { GuitarChordViewer } from '../../Utils/GuitarChordViewer';
-import { getStandardGuitarTuning } from '../../Utils/StringedInstrumentTuning';
-import { arrayContains, toggleArrayElement } from '../../../lib/Core/ArrayUtils';
+import { FlashCard, FlashCardSide, createFlashCardId } from "../../../FlashCard";
+import { FlashCardSet } from "../../../FlashCardSet";
+import { getStandardGuitarTuning, StringedInstrumentTuning } from '../../Utils/StringedInstrumentTuning';
+import { StringedInstrumentNote } from "../../../lib/TheoryLib/StringedInstrumentNote";
+import { GuitarFretboard, renderGuitarNoteHighlightsAndLabels } from "../../Utils/GuitarFretboard";
+import { renderDistinctFlashCardSideAnswerSelect } from "../Utils";
 
-const flashCardSetId = "guitarChordOrderedNotes";
-const guitarTuning = getStandardGuitarTuning(6);
-const chordTypes = ChordType.All;
+const flashCardSetId = "guitarChords";
+const guitarTuning: StringedInstrumentTuning = getStandardGuitarTuning(/*stringCount*/ 6);
 
-interface IConfigData {
-  enabledChordTypes: ChordType[];
-}
-
-export function configDataToEnabledFlashCardIds(
-  flashCardSet: FlashCardSet, flashCards: Array<FlashCard>, configData: IConfigData
-): Array<FlashCardId> {
-  const newEnabledFlashCardIds = new Array<FlashCardId>();
-
-  for (let i = 0; i < chordTypes.length; i++) {
-    const chordType = chordTypes[i];
-    if (arrayContains(configData.enabledChordTypes, chordType)) {
-      newEnabledFlashCardIds.push(flashCards[i].id);
-    }
-  }
-
-  return newEnabledFlashCardIds;
-}
-
-export interface IGuitarChordsFlashCardMultiSelectProps {
-  studySessionInfo: FlashCardStudySessionInfo;
-  onChange?: (newValue: Array<FlashCardId>, newConfigData: any) => void;
-}
-export interface IGuitarChordsFlashCardMultiSelectState {}
-export class GuitarChordsFlashCardMultiSelect extends React.Component<IGuitarChordsFlashCardMultiSelectProps, IGuitarChordsFlashCardMultiSelectState> {
-  public render(): JSX.Element {
-    const configData = (this.props.studySessionInfo.configData as IConfigData);
-    const chordTypeCheckboxTableRows = chordTypes
-      .map((chordType, i) => {
-        const isChecked = configData
-          .enabledChordTypes.indexOf(chordType) >= 0;
-        const isEnabled = !isChecked || (configData.enabledChordTypes.length > 1);
-
-        return (
-          <TableRow key={i}>
-            <TableCell><Checkbox checked={isChecked} onChange={event => this.toggleChordEnabled(chordType)} disabled={!isEnabled} /></TableCell>
-            <TableCell>{chordType.name}</TableCell>
-          </TableRow>
-        );
-      }, this);
-    const chordTypeCheckboxes = (
-      <Table className="table">
-        <TableHead>
-          <TableRow>
-            <TableCell />
-            <TableCell>Chord</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {chordTypeCheckboxTableRows}
-        </TableBody>
-      </Table>
-    );
-
-    return (
-      <Grid container spacing={32}>
-        <Grid item xs={12}>{chordTypeCheckboxes}</Grid>
-      </Grid>
-    );
-  }
-  
-  private toggleChordEnabled(chordType: ChordType) {
-    const newEnabledChordTypes = toggleArrayElement(
-      (this.props.studySessionInfo.configData as IConfigData).enabledChordTypes,
-      chordType
-    );
-    
-    if (newEnabledChordTypes.length > 0) {
-      const newConfigData: IConfigData = {
-        enabledChordTypes: newEnabledChordTypes
-      };
-      this.onChange(newConfigData);
-    }
-  }
-  private onChange(newConfigData: IConfigData) {
-    if (!this.props.onChange) { return; }
-
-    const newEnabledFlashCardIds = configDataToEnabledFlashCardIds(
-      this.props.studySessionInfo.flashCardSet, this.props.studySessionInfo.flashCards, newConfigData
-    );
-    this.props.onChange(newEnabledFlashCardIds, newConfigData);
-  }
-}
-
-function createFlashCardSet(): FlashCardSet {
-  const renderFlashCardMultiSelect = (
-    info: FlashCardStudySessionInfo,
-    onChange: (newValue: Array<FlashCardId>, newConfigData: any) => void
-  ): JSX.Element => {
-    return (
-    <GuitarChordsFlashCardMultiSelect
-      studySessionInfo={info}
-      onChange={onChange}
-    />
-    );
-  };
-
+function createFlashCardSet() {
   const flashCardSet = new FlashCardSet(flashCardSetId, "Guitar Chords", createFlashCards);
-  flashCardSet.configDataToEnabledFlashCardIds = configDataToEnabledFlashCardIds;
-  flashCardSet.getInitialConfigData = (): IConfigData => ({
-    enabledChordTypes: chordTypes
-      .filter((_, chordIndex) => chordIndex <= 8)
-  });
-  flashCardSet.renderFlashCardMultiSelect = renderFlashCardMultiSelect;
   flashCardSet.renderAnswerSelect = renderDistinctFlashCardSideAnswerSelect;
-  flashCardSet.createFlashCardLevels = (flashCardSet: FlashCardSet, flashCards: Array<FlashCard>) => (
-    chordTypeLevels
-      .map(ctl =>
-        new FlashCardLevel(
-          ctl.name,
-          flashCards
-            .filter(fc => arrayContains(ctl.chordTypes, fc.backSide.data as ChordType))
-            .map(fc => fc.id),
-          (curConfigData: IConfigData) => ({
-            enabledChordTypes: ctl.chordTypes.slice()
-          } as IConfigData)
-        )
-      )
-  );
-
   return flashCardSet;
 }
+
+const flashCardsData = [
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/3),
+      higherStringFretOffsets: [-1, -3, -2, -3]
+    },
+    backSide: "Open C Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/0),
+      higherStringFretOffsets: [2, 2, 2, 0]
+    },
+    backSide: "Open A Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/0),
+      higherStringFretOffsets: [2, 2, 1, 0]
+    },
+    backSide: "Open A Minor"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/3),
+      higherStringFretOffsets: [-1, -3, -3, -3, 0]
+    },
+    backSide: "Open G Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/0),
+      higherStringFretOffsets: [2, 2, 1, 0, 0]
+    },
+    backSide: "Open E Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/0),
+      higherStringFretOffsets: [2, 2, 0, 0, 0]
+    },
+    backSide: "Open E Minor"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/2, /*fretNumber*/0),
+      higherStringFretOffsets: [2, 3, 2]
+    },
+    backSide: "Open D Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/2, /*fretNumber*/0),
+      higherStringFretOffsets: [2, 3, 1]
+    },
+    backSide: "Open D Minor"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 2, 1, 0, 0]
+    },
+    backSide: "Barred A Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 2, 0, 0, 0]
+    },
+    backSide: "Barred A Minor"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 0, 1, 0, 0]
+    },
+    backSide: "Barred A7"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 0, 0, 0, 0]
+    },
+    backSide: "Barred Am7"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/0, /*fretNumber*/5),
+      higherStringFretOffsets: [1, 0, 0, undefined, undefined]
+    },
+    backSide: "Barred Am7b5"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 2, 2, 0]
+    },
+    backSide: "Barred D Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 2, 1, 0]
+    },
+    backSide: "Barred D Minor"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 0, 2, 0]
+    },
+    backSide: "Barred D7"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 0, 1, 0]
+    },
+    backSide: "Barred Dm7"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/1, /*fretNumber*/5),
+      higherStringFretOffsets: [1, 0, 1]
+    },
+    backSide: "Barred Dm7b5"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/2, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 3, 2]
+    },
+    backSide: "G Major"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/2, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 3, 1]
+    },
+    backSide: "G Minor"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/2, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 1, 2]
+    },
+    backSide: "G7"
+  },
+  {
+    frontSide: {
+      lowestNote: guitarTuning.getNote(/*stringIndex*/2, /*fretNumber*/5),
+      higherStringFretOffsets: [2, 1, 1]
+    },
+    backSide: "Gm7"
+  },
+];
+
+function getGuitarNotes(
+  lowestNote: StringedInstrumentNote,
+  higherStringFretOffsets: Array<number | undefined>
+): Array<StringedInstrumentNote> {
+  return [lowestNote]
+    .concat(
+      higherStringFretOffsets
+        .map((x, i) => (x !== undefined)
+          ? guitarTuning.getNote(lowestNote.stringIndex + 1 + i, lowestNote.getFretNumber(guitarTuning) + x)
+          : undefined)
+        .filter(x => x !== undefined)
+        .map(x => x as StringedInstrumentNote)
+      );
+}
+
 export function createFlashCards(): FlashCard[] {
   const guitarStyle = { width: "100%", maxWidth: "400px" };
 
-  return chordTypes.map(chordType => {
-    const rootPitch = new Pitch(PitchLetter.F, 0, 2);
-    const pitches = chordType.getPitches(rootPitch);
-    
-    return new FlashCard(
-      createFlashCardId(flashCardSetId, { chord: chordType.name }),
+  return flashCardsData
+    .map((d, i) => new FlashCard(
+      createFlashCardId(flashCardSetId, i + '.' + d.backSide),
       new FlashCardSide(
-        size => {
-          return (
-            <GuitarChordViewer
-              chordType={chordType}
-              rootPitch={rootPitch}
-              tuning={guitarTuning}
-              size={new Size2D(400, 140)}
-              style={guitarStyle}
-            />
-          )
-        },
-        pitches
+        size => (
+          <GuitarFretboard
+            width={400} height={140}
+            tuning={guitarTuning}
+            minFretNumber={0} fretCount={11}
+            renderExtrasFn={metrics => renderGuitarNoteHighlightsAndLabels(
+              metrics,
+              getGuitarNotes(d.frontSide.lowestNote, d.frontSide.higherStringFretOffsets),
+              "lightblue",
+              (n, i) => ""
+            )}
+            style={guitarStyle}
+          />
+        ),
+        d.frontSide
       ),
-      new FlashCardSide(
-        chordType.name,
-        chordType
-      )
-    );
-  });
+      new FlashCardSide(d.backSide, d.backSide)
+    ));
 }
 
 export const flashCardSet = createFlashCardSet();
